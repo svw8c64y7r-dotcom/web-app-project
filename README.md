fd49b15aec6b972c33e6559b6441fc5bc5e164b5
Sri ragav surya <svw8c64y7r@privaterelay.appleid.com>
2025-11-02 11:47:37 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
-# web-app-project
-for fun
+EPOTECK
---
+++ vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
+  server: {
+    proxy: {
+      '/signup': {
+        target: 'http://localhost:4000', // your backend server address
+        changeOrigin: true,
+        secure: false,
+      },
+      // add more routes as needed
+    },
+  },
+});
+
